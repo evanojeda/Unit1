909297a9096a65ec88a0f693df9dd8712b97a6f7
evanojeda <820185@mystma.org>
2020-09-24 20:15:59 +0000

    User Input

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,7 +1,35 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+      
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your first name.");
+        String firstname = input.nextLine();
+        
+        System.out.println("Please enter your last name.");
+        String lastname = input.nextLine();
+        
+        System.out.println("What is your age.");
+        String age = input.nextLine();
+        
+        System.out.println("What grade are you in?");
+        String grade = input.nextLine();
+        
+        System.out.println("Enter your favorite color");
+        String color = input.nextLine();
+        System.out.println("Hello ," + firstname + " " + lastname + " ." + age + " " + grade + " " + color + " ");
     }
+
+
+
+
+
+
+
+
+
 }
