9ae2e308bff4e843712b9283e0b2dc3c6fa10615
evanojeda <820185@mystma.org>
2020-09-25 20:15:45 +0000

    While Loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,6 +2,43 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        int count = 0;
+        String color = "blue";
+        boolean sport = true;
+
+    while (count < 10) {
+            
+        System.out.println("The count is " + count);
+        count = count + 1;
+        }
+        
+        System.out.println("count finished");
+    
+    while (color == "blue"){
+       System.out.println("The color is " + color);
+       color = "purple";
+    
     }
+
+        System.out.println("Color finished");
+    
+    while (sport == true)
+        System.out.println("The statment is " + sport);
+        sport = "false";
+
+    System.out.println("sport finished");
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 }
