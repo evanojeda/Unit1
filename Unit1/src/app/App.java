5a6d7b5b6a12009f522df25420b83b56a3a6dc01
evanojeda <820185@mystma.org>
2020-10-01 20:18:31 +0000

    If Loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,75 +3,9 @@ package app;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
-        int count = 0;
-        String color = "blue";
-        boolean sport = true;
+   String s1 = "ball";
+   String s2 = "puck";
 
-    while (count < 10) {
-            
-        System.out.println("The count is " + count);
-        count = count + 1;
-        }
-        
-        System.out.println("count finished");
-    
-    while (color == "blue"){
-       System.out.println("The color is " + color);
-       color = "purple";
-    
+   if (s1.equals(s2){
+       System.out.println("equial");
     }
-
-        System.out.println("Color finished");
-    
-    while (sport == true)
-        System.out.println("The statment is " + sport);
-        sport = "false";
-
-    System.out.println("sport finished");
-
-
-
-}
-=======
-      
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter your first name.");
-        String firstname = input.nextLine();
-        
-        System.out.println("Please enter your last name.");
-        String lastname = input.nextLine();
-        
-        System.out.println("What is your age.");
-        String age = input.nextLine();
-        
-        System.out.println("What grade are you in?");
-        String grade = input.nextLine();
-        
-        System.out.println("Enter your favorite color");
-        String color = input.nextLine();
-        System.out.println("Hello ," + firstname + " " + lastname + ". " + age + " " + grade + " " + color + " ");
-    }
-
-
-
-
-
->>>>>>> 92eaed87ffa38a8a1e2c2c54e8581bbe455ee415
-
-
-
-
-<<<<<<< HEAD
-
-
-
-
-
-
-}
-=======
-}
->>>>>>> 92eaed87ffa38a8a1e2c2c54e8581bbe455ee415
