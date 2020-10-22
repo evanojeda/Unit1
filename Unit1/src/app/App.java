354bff25663067346883deba1e115fa77e60c752
evanojeda <820185@mystma.org>
2020-10-22 19:47:03 +0000

    challenge 4

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,42 +3,29 @@ package app;
 import java.util.Scanner;
 
 public class App {
-<<<<<<< HEAD
     public static void main(String[] args) throws Exception {
-    int i = 0;
-    
-    
-    while(i <=1000 ){
-        System.out.println("The number is " + i );
-        i = i + 1;
+       Scanner input = new Scanner(System.in);
+
+         System.out.println("Please enter a number");
+         int num1 = input.nextInt();
+         input.nextLine();
+
+         System.out.println("Please enter a larger number");
+         int num2 = input.nextInt();
+         input.nextLine();
 
 
+     
+
+        
+
+            for (int i = num1; i < num2; i++){
+                System.out.println(i);
+               
+               
+         
+        }
+            
+   
     }
-
-
-
-    System.out.println();
-
-
-
-    }
-
 }
-=======
-   String s1 = "ball";
-   String s2 = "puck";
-
-   if (s1.equals(s2){
-       System.out.println("equal"); 
-   
-       puck = input.nextLine();
-       if(puck.equils("puck is harder than a ball");
-
-       
-
-       
-
-   
-   
-    }
->>>>>>> b1e493b054c57a18f18baf473e65164106d4d718
