87a60c3788d084b6f0fef33257c5130229771928
evanojeda <820185@mystma.org>
2020-11-06 21:14:06 +0000

    Multiply numbers

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,23 +2,30 @@ package app;
 
 import java.util.Scanner;
 
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-       Scanner input = new Scanner(System.in)
-        
-       System.out.println("Enter the temp outside");
-       int temp = input.nextInt();
-       input.nextLine();
-
-       if (temp < 32 = freezing);
-
-
-
-
-               
-         
-        }
-            
-   
+       greaterthanten(7);
+       mult();
     }
+
+public static void greaterthanten(int number){ 
+    if (number > 10){
+    System.out.println(number + "Is greater tnan 10");
+}
+    else {
+    System.out.println(number + "Is not greater than 10");
+    
+
+
+
+    }
+    public static void mult(int number, int number2, int number3, int number4){ 
+        System.out.println(mult1 * mult2 * mult3 * mult4);
+       
+    }
+mult(2, 4, 5, 8); 
+
+}
 }
