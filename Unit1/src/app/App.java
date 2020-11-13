18508c56812a9846adf81e4b15bef832257da8ae
evanojeda <820185@mystma.org>
2020-11-13 21:12:05 +0000

    Challenge 2

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,26 +6,17 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-         String person1 = "Ted";
-         String verb1 = "eat.";
-         String dayOfWeek = "friday";
-         String adjective = "huge";
-         String food = "plate of wings";
-         String verb2 = "lunch";
-         String place1 = "my house";
-         String person2 = "Bob.";
-         String adjective2 = "sports ";
-         String place2 = " restaurant";
-         String verb3 = "watch football";
-
+        Scanner input = new Scanner (System.in);
+        String password = "dalvincooks33";
+        int tries = 0;
+        boolean secretMessageGiven = false;
         
+        while (tries < 3) {
+            if (secretMessageGiven == false) {
+                System.out.println("Enter the password");
+                String enteredpassword = input.nextLine();
 
-      
-      
-      
-      
-        System.out.println(person1 + " is a programmer who likes to " + verb1);
-        System.out.println(" Every " + dayOfWeek + " they make a " + adjective + food + " for breakfast and " + verb2 + " at " + place1 + " with " + person2);
-        System.out.println(" Sometimes, they go to the " + adjective2 + place2 + " with " + person2 + " so they can " + verb3 + " together. ");
-}
+                
+
+
 }
