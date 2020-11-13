9c37a833696498b121480b290ffb6b565ca13e4b
evanojeda <820185@mystma.org>
2020-11-13 20:59:30 +0000

    Mab 1

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,26 +6,26 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       greaterthanten(7);
-       mult();
-    }
+         String person1 = "Ted";
+         String verb1 = "eat.";
+         String dayOfWeek = "friday";
+         String adjective = "huge";
+         String food = "plate of wings";
+         String verb2 = "lunch";
+         String place1 = "my house";
+         String person2 = "Bob.";
+         String adjective2 = "sports ";
+         String place2 = " restaurant";
+         String verb3 = "watch football";
 
-public static void greaterthanten(int number){ 
-    if (number > 10){
-    System.out.println(number + "Is greater tnan 10");
-}
-    else {
-    System.out.println(number + "Is not greater than 10");
-    
+        
 
-
-
-    }
-    public static void mult(int number, int number2, int number3, int number4){ 
-        System.out.println(mult1 * mult2 * mult3 * mult4);
-       
-    }
-mult(2, 4, 5, 8); 
-
+      
+      
+      
+      
+        System.out.println(person1 + " is a programmer who likes to " + verb1);
+        System.out.println(" Every " + dayOfWeek + " they make a " + adjective + food + " for breakfast and " + verb2 + " at " + place1 + " with " + person2);
+        System.out.println(" Sometimes, they go to the " + adjective2 + place2 + " with " + person2 + " so they can " + verb3 + " together. ");
 }
 }
