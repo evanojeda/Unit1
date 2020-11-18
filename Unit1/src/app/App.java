0e1be9a7e1be4783b2b7f84ad508efbf3b088c29
evanojeda <820185@mystma.org>
2020-11-18 18:17:12 +0000

    Challenge 2

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner (System.in);
-        String password = "dalvincooks33";
+        String password = "Evansabeast";
         int tries = 0;
         boolean secretMessageGiven = false;
         
@@ -15,6 +15,31 @@ public class App {
             if (secretMessageGiven == false) {
                 System.out.println("Enter the password");
                 String enteredpassword = input.nextLine();
+                
+                 if (enteredpassword.equals(password)) {
+                     System.out.println("Access Granted");
+                     System.out.println("Dont forget to eat your crust");
+                     secretMessageGiven = true; }
+
+                else {
+                    System.out.println("Access Denied");
+                    tries++;
+                    System.out.println("You have " + (3 - tries) + " Attempts Remaining");
+                }
+            }
+        }
+    if (tries== 3) {
+        System.out.println("Too many attempts");
+    }
+
+}
+
+}
+
+
+                
+
+
 
                 
 
