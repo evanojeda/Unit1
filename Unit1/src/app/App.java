e27043db3878cacfd1dafc30fa466cb0c5b27681
evanojeda <820185@mystma.org>
2020-11-19 21:12:45 +0000

    Challenge 3

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,31 +6,46 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner (System.in);
-        String password = "dalvincooks33";
-        int tries = 0;
-        boolean secretMessageGiven = false;
         
-        while (tries < 3) {
-            if (secretMessageGiven == false) {
-                System.out.println("Enter the password");
-                String enteredpassword = input.nextLine();
-                
-                 if (enteredpassword.equals(password)) {
-                     System.out.println("Access Granted");
-                     System.out.println("Dont forget to eat your crust");
-                     secretMessageGiven = true; }
+        Scanner input = new Scanner(System.in);
 
-                else {
-                    System.out.println("Access Denied");
-                    tries++;
-                    System.out.println("You have " + (3 - tries) + " Attempts Remaining");
-                }
+        int targetNum = 72;
+        boolean isFinished = false;
+
+        while (isFinished == false) {
+        
+
+             System.out.println("Guess a number between 0 and 100");
+             int guess = input.nextInt();
+             input.nextLine();
+
+             if (guess == targetNum) {
+                 System.out.println("Correct");
+                 isFinished = true;
+
+             }
+
+             else if (guess < targetNum) {
+                 System.out.println("Low");
+
+             }
+
+             else {
+                 System.out.println("High");
+
+
+             }
             }
+
         }
-    if (tries== 3) {
-        System.out.println("Too many attempts");
-    }
+
+    
+    
+    
+    
+    
+        
+
 
 }
 
